9b70e482f7eaabef0b845c7a45a68de4e09a8cf0
Rishabh gupta <rishabhgupta1106@gmail.com>
2020-07-28 18:22:43 +0530

    Small change

--- src/main/java/com/CItest/CIDemo/CiDemoApplication.java
+++ src/main/java/com/CItest/CIDemo/CiDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CiDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.print("Small change");
 		SpringApplication.run(CiDemoApplication.class, args);
 	}
 
