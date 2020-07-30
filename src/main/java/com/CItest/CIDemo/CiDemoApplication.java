1d3b15516667e29485dbf840cde20e9c81fd7770
Rishabh gupta <rishabhgupta1106@gmail.com>
2020-07-30 11:23:49 +0530

    6

--- src/main/java/com/CItest/CIDemo/CiDemoApplication.java
+++ src/main/java/com/CItest/CIDemo/CiDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CiDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.print("error removed-!@#12345");
+		System.out.print("error removed-!@#123456");
 		SpringApplication.run(CiDemoApplication.class, args);
 	}
 
